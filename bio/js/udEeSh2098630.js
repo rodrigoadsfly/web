9d66f3e0791a7baf676bf35aa547ecf96cac7d46
rodrigoadsfly <rodrigoadsfly@gmail.com>
2025-01-12 22:13:53 -0300

    Add files via upload

---
+++ bio/js/udEeSh2098630.js
@@ -0,0 +1,140 @@
+
+    function atomiApplyParams({inputUrl}) {
+      try {
+        console.log(inputUrl)
+        const inputUrlObj = new URL(inputUrl, window.location.origin);
+        const currentPageParams = new URLSearchParams(window.location.search);
+        const inputUrlParams = new URLSearchParams(inputUrlObj.search);
+      
+        // Iterate over all parameters in the current page's URL
+        for (const [key, value] of currentPageParams) {
+          // If the input URL does not already contain the parameter, add it
+          if (!inputUrlParams.has(key)) {
+            inputUrlParams.append(key, value);
+          }
+        }
+      
+        // Construct the final URL
+        const finalUrl = inputUrlObj.origin + inputUrlObj.pathname + '?' + inputUrlParams.toString();
+        console.log(finalUrl)
+        return finalUrl;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    function atomiFormatDate(options = { slated: false, addDate: 0 }) {
+      try {
+        const defaultOptions = {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        };
+
+        const today = new Date();
+
+        if (options.slated) {
+          const slatedDate = new Date(today);
+          slatedDate.setDate(slatedDate.getDate() + (options.addDate || 0));
+
+          const day = slatedDate.getDate().toString().padStart(2, "0");
+          const month = (slatedDate.getMonth() + 1).toString().padStart(2, "0");
+          const year = slatedDate.getFullYear();
+          return `${day}/${month}/${year}`;
+        }
+
+        if(options.addDate){
+          today.setDate(today.getDate()+options.addDate)
+        }
+        const formattedDate = today.toLocaleDateString(undefined, defaultOptions);
+
+        return formattedDate;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    function atomiFormatTime() {
+      try {
+        const now = new Date();
+        return now.toLocaleTimeString(undefined, {
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    function runDelayedFunctions(data) {
+      try {
+        document.querySelectorAll('.atomicat-delay').forEach(el => el.classList.remove('atomicat-delay'));
+        if(data?.setDisplayed){
+          localStorage.setItem(data?.setDisplayed, true);
+        }
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  (function() {
+          try {
+              const clickeventList = [{"compKey":"45b71585-9496-42fc-92c9-ff2ee261a548","misc":{"type":"image"}},{"compKey":"8218fd08-3d24-4d0c-9347-11a5aeae449a","misc":{"type":"image"}}];
+    
+    
+              clickeventList.forEach((comp, index) => {
+                  const compKey = comp?.compKey?.slice(0, 7);
+                  const eleType = comp?.misc?.type;
+                  const showItemsById = comp?.misc?.showItemsById;
+                  const hideAfterClick = comp?.misc?.hideAfterClick;
+                  const hideOnComplete = comp?.misc?.hideOnComplete;
+                  console.log(comp, "clickevent")
+                  if(hideAfterClick) {
+                    const hideAfterClickEle = document.querySelector(`.atomicat-hide-after-click-${compKey}`);
+                    console.log(hideAfterClickEle, "hideAfterClickEle")
+                    if (hideAfterClickEle) {
+                      hideAfterClickEle.addEventListener("click", function() {
+                          console.log("hideAfterClickEle clicked")
+                          hideAfterClickEle.classList.add("atomicat-hidden");
+                      })
+                    }
+                  }
+                  if(hideOnComplete) {
+                    const hideOnCompleteEle = document.querySelector(`.atomicat-hide-on-complete-${compKey}`);
+                    console.log(hideOnCompleteEle, "hideOnCompleteEle")
+                    if (hideOnCompleteEle) {
+                      hideOnCompleteEle.addEventListener("animationend", function() {
+                          console.log("hideOnCompleteEle animationend")
+                          hideOnCompleteEle.classList.add("atomicat-hidden");
+                      })
+                    }
+                  }
+                  if(showItemsById) {
+                    const showItemsByIdEle = document.querySelector(`.atomicat-show-hidden-item-${compKey}`);
+                    if(eleType === "progressbar"){
+                      showItemsByIdEle.addEventListener("animationend", function() {
+                        console.log("animation end")
+                        atomiShowItems()
+                      })
+                    } else{
+                      showItemsByIdEle.addEventListener("click", function() {
+                        console.log("showItemsByIdEle click")
+                        atomiShowItems()
+                      })
+                    }
+                    function atomiShowItems() {
+                      showItemsById.forEach((item) => {
+                        const hiddenItem = document.querySelector(`#${item}`) || document.querySelector(`.${item}`);
+                        if (hiddenItem) {
+                          hiddenItem.classList.remove("atomicat-delay");
+                        }
+                      })
+                    }
+                  }
+              });
+    
+          } catch (error) {
+              console.log(error);
+          }
+      })();
